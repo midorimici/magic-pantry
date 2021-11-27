0f9aad7347fe08f6058523f88c3e78ff93bde676
mici <60635066+midorimici@users.noreply.github.com>
2021-11-27 22:53:38 +0900

    ✨ #2 Add `AddIngredientRecommendation` component

---
+++ src/components/organisms/AddIngredientRecommendation.tsx
@@ -0,0 +1,17 @@
+import { Button, Stack, Typography } from '@mui/material'
+import { Add } from '@mui/icons-material'
+
+type Props = {
+  setShowDialog: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+export const AddIngredientRecommendation: React.FC<Props> = ({ setShowDialog }) => {
+  return (
+    <Stack alignItems="center" gap={2} justifyContent="center" sx={{ height: '80vh' }}>
+      <Typography>{`You don't have any ingredients yet.`}</Typography>
+      <Button startIcon={<Add />} variant="contained" onClick={() => setShowDialog(true)}>
+        Add ingredients
+      </Button>
+    </Stack>
+  )
+}
--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -1,1 +1,2 @@
+export { AddIngredientRecommendation } from './AddIngredientRecommendation'
 export { SignInForm } from './SignInForm'

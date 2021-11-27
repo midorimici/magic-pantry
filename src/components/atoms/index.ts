837759dbcd6f5e7addc30ab0432d9ebbf4e27b65
mici <60635066+midorimici@users.noreply.github.com>
2021-11-28 00:12:21 +0900

    ✨ #3 Add `IngredientCard` component

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,3 +1,4 @@
 export { HeaderContents } from './HeaderContents'
 export type { HeaderContentsProps } from './HeaderContents'
+export { IngredientCard } from './IngredientCard'
 export { Meta } from './Meta'

2c1220fd296386183713d2d2319c9ae79d8cc8c2
mici <60635066+midorimici@users.noreply.github.com>
2021-12-02 23:37:48 +0900

    ♻️ #2 Toggle display inside `AddIngredientRecommendation`

--- src/components/templates/Pantry.tsx
+++ src/components/templates/Pantry.tsx
@@ -3,19 +3,14 @@ import { Meta } from 'components/atoms'
 import { Container, HeaderProps } from 'components/molecules'
 import { AddIngredientDialog, AddIngredientRecommendation } from 'components/organisms'
 
-type Props = {
-  /** There is no ingredient in the pantry */
-  noIngredient: boolean
-} & HeaderProps
-
-export const Pantry: React.FC<Props> = ({ noIngredient, ...props }) => {
+export const Pantry: React.FC<HeaderProps> = (props) => {
   const [showDialog, setShowDialog] = useState(false)
 
   return (
     <>
       <Meta title="Pantry" />
       <Container {...props}>
-        {noIngredient && <AddIngredientRecommendation setShowDialog={setShowDialog} />}
+        <AddIngredientRecommendation setShowDialog={setShowDialog} />
         <AddIngredientDialog showDialog={showDialog} setShowDialog={setShowDialog} />
       </Container>
     </>
--- src/pages/pantry.tsx
+++ src/pages/pantry.tsx
@@ -3,11 +3,9 @@ import type { NextPage } from 'next'
 import { Pantry } from 'components/templates'
 import { useAuthState, useSignOutHandler } from 'hooks'
 import { authUserState } from 'states/auth/atom'
-import { ingredientsState } from 'states/pantry/atom'
 
 const PantryPage: NextPage = () => {
   const user = useRecoilValue(authUserState)
-  const ingredients = useRecoilValue(ingredientsState)
   const { isLoading, handleSignOut } = useSignOutHandler()
   const { isLoadingAuthState } = useAuthState()
 
@@ -15,7 +13,6 @@ const PantryPage: NextPage = () => {
     <Pantry
       isLoading={isLoading || isLoadingAuthState}
       isLoggedIn={user !== null}
-      noIngredient={ingredients.length === 0}
       handleSignOutButtonClick={handleSignOut}
     />
   )

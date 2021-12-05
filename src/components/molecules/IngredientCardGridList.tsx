283e5b0a407d8df0352c573957e2afe3abdf2d5f
mici <60635066+midorimici@users.noreply.github.com>
2021-12-05 21:10:23 +0900

    💄 #3 Update width of `Container`

--- src/components/molecules/IngredientCardGridList.tsx
+++ src/components/molecules/IngredientCardGridList.tsx
@@ -9,7 +9,7 @@ export const IngredientCardGridList: React.FC<Props> = ({ ings }) => {
   return (
     <Grid container spacing={2}>
       {ings.map((ing: Ingredient, index: number) => (
-        <Grid key={index} item xs={4}>
+        <Grid key={index} item xs={3}>
           <IngredientCard ing={ing} />
         </Grid>
       ))}
--- src/components/molecules/IngredientCardSkeleton.tsx
+++ src/components/molecules/IngredientCardSkeleton.tsx
@@ -3,8 +3,8 @@ import { Grid, Skeleton } from '@mui/material'
 export const IngredientCardSkeleton: React.FC = () => {
   return (
     <Grid container spacing={2}>
-      {[...Array(3)].map((_, index: number) => (
-        <Grid key={index} item xs={4}>
+      {[...Array(4)].map((_, index: number) => (
+        <Grid key={index} item xs={3}>
           <Skeleton height={150} />
         </Grid>
       ))}

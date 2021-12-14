1a59ab2991235445e49299ffa3852bec900d2538
mici <60635066+midorimici@users.noreply.github.com>
2021-12-14 21:03:56 +0900

    📱 Responsive support for Grid

--- src/components/molecules/CardGridListSkeleton.tsx
+++ src/components/molecules/CardGridListSkeleton.tsx
@@ -4,7 +4,7 @@ export const CardGridListSkeleton: React.FC = () => {
   return (
     <Grid container spacing={2}>
       {[...Array(4)].map((_, index: number) => (
-        <Grid key={index} item xs={3}>
+        <Grid key={index} item xs={12} sm={4} md={3}>
           <Skeleton height={150} />
         </Grid>
       ))}
--- src/components/molecules/IngredientCardGridList.tsx
+++ src/components/molecules/IngredientCardGridList.tsx
@@ -10,7 +10,7 @@ export const IngredientCardGridList: React.FC<Props> = ({ ings, onCardClick }) =
   return (
     <Grid container spacing={2}>
       {Object.entries(ings).map(([key, ing]: [string, Ingredient]) => (
-        <Grid key={key} item xs={3}>
+        <Grid key={key} item xs={12} sm={4} md={3}>
           <IngredientCard id={key} ing={ing} onClick={onCardClick} />
         </Grid>
       ))}

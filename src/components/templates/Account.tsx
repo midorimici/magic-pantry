d1824b3d4d17158709540e24a7975e192e0a0642
mici <60635066+midorimici@users.noreply.github.com>
2021-12-18 15:48:10 +0900

    ✨ #30 Add account setting components to account page

--- src/components/templates/Account.tsx
+++ src/components/templates/Account.tsx
@@ -1,4 +1,6 @@
+import { Stack } from '@mui/material'
 import { Meta, PageTitle } from 'components/atoms'
+import { ChangePasswordForm, DeleteAccountButton } from 'components/organisms/account'
 import { Container, HeaderProps } from 'components/organisms/container'
 
 export const Account: React.FC<HeaderProps> = (props) => {
@@ -7,6 +9,10 @@ export const Account: React.FC<HeaderProps> = (props) => {
       <Meta title="🔧 Account Settings" />
       <Container {...props}>
         <PageTitle title="🔧 Account Settings" />
+        <Stack alignItems="flex-start" gap={8}>
+          <ChangePasswordForm />
+          <DeleteAccountButton />
+        </Stack>
       </Container>
     </>
   )
